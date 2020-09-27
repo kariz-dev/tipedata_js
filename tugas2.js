b8a8246525007e8468e20e0557b331d5c56f822b
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2020-09-27 19:56:57 +0700

    tugas 2

---
+++ tugas2.js
@@ -0,0 +1,37 @@
+// Integer atau Number
+var number = 2;
+
+// String
+var string = "Muhammad Rizwan"
+
+// Boolean
+var boolean = false
+var meter = 150
+
+if(meter>130){
+    boolean= true
+}else {
+    boolean = false
+}
+
+// Null
+var nilaiKosong = null
+
+// Undefined
+var gakAdaNilai
+
+// Array
+var aku = ["Muhammad Rizwan", 24, "Jakarta Utara", "traveling"]
+
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(nilaiKosong)
+console.log(gakAdaNilai)
+console.log(
+    "Nama",(aku[0]),
+    "| Umur",(aku[1]),
+    "| Alamant",(aku[2]),
+    "| Hobi",(aku[3])
+    )
